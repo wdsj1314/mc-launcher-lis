f313599197ed6137ed58c6be05769904744926d3
神 <VRWDSJ1314@163.com>
2026-08-10 10:42:06 +0800

    Modify listData structure and content
    Updated listData to include new entries and removed old ones.

---
+++ list.js
@@ -0,0 +1,12 @@
+const listData = [
+  {
+    name: "ZL2(ZalithLauncher2)",
+    desc: "安卓Minecraft Java启动器",
+    goUrl: "https://zalithlauncher.cn"
+  },
+  {
+    name: "FCL启动器",
+    desc: "MC Java安卓启动器",
+    goUrl: "https://github.com/FCL‑Team/FCL‑Launcher"
+  }
+];
